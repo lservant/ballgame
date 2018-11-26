68066d3184a2fc6d1372e07f11bd351750d30fec
Louis Servant <louservant@gmail.com>
2018-11-26 07:44:58 -0600

    start box generation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import $ from "jquery";
+import style from "./styles.scss";
 
-function component() {
-    let $playarea = $('#playarea');
-    $playarea.text('Hello world 3');
+let $pa = $('#playarea');
+let colors = ['red', 'yellow', 'blue', 'orange', 'green', 'purple'];
+
+function init() {
+    let blockcon = '<div class="block-container"></div>';
+    let block = '<div class="block"></div>';
+    let row = '<div class="row"></div>';
+    $pa.append(blockcon);
+    for (let i = 0; i < 3; i++) {
+        var $r = $('.block-container').append(row).find('.row:last');
+        for (let j = 0; j < 10; j++) {
+            $r.append(block).find('.block:last').css('background', colors[j % colors.length]);
+        }
+    }
 }
-component();
+
+init();
